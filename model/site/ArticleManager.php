2dbaf14b631d483814406133168a9f49239891ce
kaloyansen <kalojanssen@gmail.com>
2022-03-26 14:48:40 +0100

    auto-commit by Kaloyan KRASTEV

--- model/site/ArticleManager.php
+++
@@ -1,9 +0,0 @@
-<?php namespace model\site;
-class ArticleManager extends \model\BaseManager {/* conception
-	et description*/
-	public function create(Article $article) { return; }
-	public function readAll() {	return;	}
-	public function read($id) { return; }
-	public function update(Article $article) { return; }
-	public function delete(Article $article) { return; }
-} ?>
